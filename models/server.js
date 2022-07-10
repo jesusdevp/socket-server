cf98a7ee9549a900bfdfa8bbbf94b01f3b8515a4
Jesus Ramirez <jesusdevelp@gmail.com>
2022-07-09 23:00:52 -0500

    Socket server

---
+++ models/server.js
@@ -0,0 +1,53 @@
+// Servidor de Express
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const path = require('path')
+const cors = require('cors')
+
+const Sockets = require('./sockets')
+
+class Server {
+
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT;
+
+        // HTTP server
+        this.server = http.createServer(this.app)
+
+        
+        // Configuracion del socket server
+        this.io = socketio(this.server)
+
+        // CORS
+        this.app.use( cors() );
+
+    }
+
+    middlewares() {
+        // Desplegar el directorio publico
+        this.app.use(express.static(path.resolve(__dirname, '../public')))
+    }
+
+    configurarSockets() {
+        new Sockets(this.io)
+    }
+
+    execute() {
+        
+        // Inicializar middlewares
+        this.middlewares()
+
+        // Inicializar sockets
+        this.configurarSockets();
+
+        // inicializar server
+        this.server.listen(this.port ,() => {
+            console.log('Servidor corriendo en puerto:', this.port)
+        });
+    }
+
+}
+
+module.exports = Server
